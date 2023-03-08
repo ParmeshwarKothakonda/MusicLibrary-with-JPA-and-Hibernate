1eb5d73961031f97b1e7c09473260b27e86501dd
parmeshwar <parmeshwar8121@gmail.com>
2023-03-08 06:29:08 +0000

    solution

---
+++ src/main/java/com/example/song/model/Song.java
@@ -0,0 +1,76 @@
+package com.example.song.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "playlist")
+public class Song{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="songid")
+    private int songId;
+
+    @Column(name="songname")
+    private String songName;
+
+    @Column(name="lyricist")
+    private String lyricist;
+
+    @Column(name="singer")
+    private String singer;
+
+    @Column(name="musicdirector")
+    private String musicDirector;
+
+    public Song(int songId, String songName, String lyricist, String singer, String musicDirector){
+        this.songId = songId;
+        this.songName = songName;
+        this.lyricist = lyricist;
+        this.singer = singer;
+        this.musicDirector = musicDirector;
+    }
+
+    public Song(){} //Default Constructor
+
+    public void setSongId(int songId){ 
+        this.songId = songId;
+    }
+
+    public int getSongId(){ //Getter
+        return songId;
+    }
+
+    public void setSongName(String songName){
+        this.songName = songName;
+    }
+
+    public String getSongName(){ //getter
+        return songName;
+    }
+
+    public void setLyricist(String lyricist){
+        this.lyricist = lyricist;
+    }
+
+    public String getLyricist(){ //getter
+        return lyricist;
+    }
+
+    public void setSinger(String singer){
+        this.singer = singer;
+    }
+
+    public String getSinger(){ //getter
+        return singer;
+    }
+
+    public void setMusicDirector(String musicDirector){
+        this.musicDirector = musicDirector;
+    }
+
+    public String getMusicDirector(){ //getter
+        return musicDirector;
+    }
+
+}
