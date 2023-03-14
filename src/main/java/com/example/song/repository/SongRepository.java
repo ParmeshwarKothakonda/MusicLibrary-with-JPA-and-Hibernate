604460990c84e9dad8a8557891aa717eb18dd217
Parmeshwar Kothakonda <87478621+ParmeshwarKothakonda@users.noreply.github.com>
2023-03-14 16:14:59 +0530

    Added Post method for multiple songs

--- src/main/java/com/example/song/repository/SongRepository.java
+++ src/main/java/com/example/song/repository/SongRepository.java
@@ -6,6 +6,7 @@ import java.util.*;
 public interface SongRepository{
     ArrayList<Song> getSongs();
     Song addSong(Song song);
+    String addMultipleSongs(ArrayList<Song> multipleSongs);
     Song getSongById(int songId);
     Song updateSong(int songId, Song song);
     void deleteSong(int songId);
